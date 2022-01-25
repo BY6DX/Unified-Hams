6e75346f8158a6121cbe91994534ebe24f4e9f92
Libre Liu <libreliu@foxmail.com>
2022-01-25 12:55:17 +0800

    Add admins

--- backend/station/admin.py
+++ backend/station/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Station, HomeStation, Antenna, Transceiver
 
 # Register your models here.
+admin.site.register(Station)
+admin.site.register(HomeStation)
+admin.site.register(Antenna)
+admin.site.register(Transceiver)
